6d23c2cc4d20a049743f4d38f9ca8a54762b0782
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-04-23 13:38:48 +1000

    Changed href Nav.Link in TopNav.js

--- src/components/top-nav/TopNav.js
+++ src/components/top-nav/TopNav.js
@@ -12,8 +12,8 @@ export const TopNav = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Skills</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#about">About Me</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
